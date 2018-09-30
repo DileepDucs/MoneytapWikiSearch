798fa5c99e4ccea451d8d8552192ab3d6ad6a748
Amrita Jaiswal <manjujaiswal96@gmail.com>
2018-09-30 23:31:24 +0530

    Initial Commit

---
+++ WikiInfoSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WikiInfoSearch
+//
+//  Created by Dileep Kumar Jaiswal on 30/09/18.
+//  Copyright © 2018 Dileep Kumar Jaiswal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
